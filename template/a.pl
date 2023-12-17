5794c53fa4daba77de9e7fd01f195ea8729641dc
Ivan Bessarabov <ivan@bessarabov.ru>
2023-12-17 14:52:56 +0000

    9-1

--- template/a.pl
+++ template/a.pl
@@ -6,29 +6,37 @@ use feature 'say';
 use utf8;
 use open qw(:std :utf8);
 
+use Moment;
 use Test::More;
 use Data::Dumper;
 
+sub read_file {
+    my ($file_name) = @_;
 
-sub main {
+    my $content = '';
 
-    my $sum = 0;
-
-    my $file = 'input';
-    #my $file = 'i';
-    open(my $fh, '<', $file) or die "Could not open file '$file' $!";
-
+    open(my $fh, '<', $file_name) or die "Could not open file '$file_name' $!";
 
     while (my $line = <$fh>) {
-        chomp($line);
-
+        $content .= $line;
     }
 
     close($fh);
 
-#    is($sum, );
+    return $content;
+}
 
-    say $sum;
+sub main {
+
+    $Data::Dumper::Sortkeys = 1;
+
+    my $answer = 0;
+
+    my $file_name = $ARGV[0];
+    my $content = read_file($file_name);
+
+
+    say $answer;
 
 }
 main();
