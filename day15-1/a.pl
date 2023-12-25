96c4f44a231cb5336bcd64869c69ad821b5778a7
Ivan Bessarabov <ivan@bessarabov.ru>
2023-12-25 19:14:18 +0000

    15-1

---
+++ day15-1/a.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings FATAL => 'all';
+use feature 'say';
+use utf8;
+use open qw(:std :utf8);
+
+use Moment;
+use Test::More;
+use Data::Dumper;
+
+$Data::Dumper::Sortkeys = 1;
+
+sub p {
+    say Dumper $_[0];
+}
+
+sub read_file {
+    my ($file_name) = @_;
+
+    my $content = '';
+
+    open(my $fh, '<', $file_name) or die "Could not open file '$file_name' $!";
+
+    while (my $line = <$fh>) {
+        $content .= $line;
+    }
+
+    close($fh);
+
+    return $content;
+}
+
+=head2 get_field
+
+$field[0] - first row
+$field[0]->[2] - 3rd element if 1st row
+
+$field[$y]->[$x]
+
+(x,y)
+
+0,0     1,0     2,0     # $field[0]
+0,1     1,1     2,1     # $field[1]
+
+    my $max_x = scalar(@{$field[0]})-1;
+    my $max_y = scalar(@field)-1;
+
+    foreach my $x (0..$max_x) {
+        foreach my $y (0..$max_y) {
+            #
+        }
+    }
+
+=cut
+
+sub get_field {
+    my ($content) = @_;
+
+    my @field;
+
+    foreach my $row (split /\n/, $content) {
+        my @elements = split //, $row;
+        push @field, \@elements;
+    }
+
+    return @field;
+}
+
+sub min {
+    my ($one, $two) = @_;
+
+    return $one < $two ? $one : $two;
+}
+
+sub max {
+    my ($one, $two) = @_;
+
+    return $one > $two ? $one : $two;
+}
+
+sub get_hash_value {
+    my ($str) = @_;
+
+    my $i = 0;
+    foreach my $char (split //, $str) {
+        $i += ord($char);
+        $i *= 17;
+        $i = $i % 256;
+    }
+
+    return $i;
+}
+
+sub main {
+
+    my $file_name = $ARGV[0];
+    my $content = read_file($file_name);
+    chomp($content);
+
+    my @elements = split /,/, $content;
+
+    my $i = 0;
+    foreach my $el (@elements) {
+        $i += get_hash_value($el);
+    }
+
+    say $i;
+
+}
+main();
+__END__
